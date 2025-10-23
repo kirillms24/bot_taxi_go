17b17d2b96a7b5a7a974fc6a6583c8e0108e632e
kirillms24 <124241683+kirillms24@users.noreply.github.com>
2025-10-24 00:07:49 +0500

    Create models.py

---
+++ backend/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy.sql import func
+from database import Base
+
+class Ticket(Base):
+    __tablename__ = "tickets"
+
+    id = Column(Integer, primary_key=True, index=True)
+    customer_name = Column(String, index=True)
+    message = Column(Text)
+    response = Column(Text, default="")
+    status = Column(String, default="Новый")  # Новый, В работе, Закрыт
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
